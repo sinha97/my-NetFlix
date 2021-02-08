9034b10e76569f48be9175b47b5a52496e9d4bf9
sinha97 <vk9633698@gmail.com>
2021-02-08 08:22:55 +0530

    some added header

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -22,8 +22,16 @@ const HomeScreen = () => {
                 fetchUrl={requests.fetchTrending}
             />
             <Row
+                title="Top Rated"
+                fetchUrl={requests.fetchTopRated}
+            />
+            <Row
                 title="Action Movies"
                 fetchUrl={requests.fetchActionMovies}
+            />
+            <Row
+                title="Comedy Movies"
+                fetchUrl={requests.fetchComedyMovies}
             />
             <Row
                 title="Horror Movies"
